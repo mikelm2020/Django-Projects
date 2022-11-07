3fbcc2d8903ed1a5681224bd0349c4848380a85e
Miguel Angel López <admon@tecnott.com.mx>
2022-11-07 15:23:59 -0600

    Deleted project video_streaming

--- video_streaming/streaming/applications/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "core_app"
-
-urlpatterns = [
-    path('', views.HomePage.as_view(), name='user_panel'),
-]
--- video_streaming/streaming/applications/users/functions.py
+++
@@ -1,8 +0,0 @@
-# Extra functions of the app users
-
-import random
-import string
-
-
-def code_generator(size=6, chars=string.ascii_uppercase + string.digits):
-    return "".join(random.choice(chars) for _ in range(size))
